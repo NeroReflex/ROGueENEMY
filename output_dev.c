4cde4a3812ad29f1ac178a7b0ce5a14d8f027965
Denis <benato.denis96@gmail.com>
2023-11-11 19:28:07 +0100

    BTN_BACK

--- output_dev.c
+++ output_dev.c
@@ -165,7 +165,7 @@ int create_output_dev(const char* uinput_path, const char* name, output_dev_type
 			ioctl(fd, UI_SET_KEYBIT, BTN_SELECT);
 			ioctl(fd, UI_SET_KEYBIT, BTN_START);
 			ioctl(fd, UI_SET_KEYBIT, BTN_MODE);
-			//ioctl(fd, UI_SET_KEYBIT, BTN_TRIGGER);
+			ioctl(fd, UI_SET_KEYBIT, BTN_BACK);
 			ioctl(fd, UI_SET_KEYBIT, BTN_THUMBL);
 			ioctl(fd, UI_SET_KEYBIT, BTN_THUMBR);
 
@@ -372,7 +372,7 @@ void *output_dev_thread_func(void *ptr) {
 				if ((ev.type == EV_KEY) && (ev.code == KEY_F16)) {
 					ev.code = BTN_MODE;
 				} else if ((ev.type == EV_KEY) && (ev.code == KEY_PROG1)) { // To be wired to F16
-					ev.code = BTN_TR2;
+					ev.code = BTN_BACK;
 				} else if ((ev.type == EV_KEY) && (ev.code == KEY_F18)) { // To be wired to F16
 					ev.code = BTN_GEAR_UP;
 				} else if ((ev.type == EV_KEY) && (ev.code == KEY_F17)) { // To be wired to F16
