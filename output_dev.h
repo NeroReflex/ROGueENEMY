5c576d810bb5bca33e5fe3828b55473b5b7afe34
Denis <benato.denis96@gmail.com>
2023-11-13 01:04:38 +0100

    DualShock controller

--- output_dev.h
+++ output_dev.h
@@ -10,26 +10,31 @@
 #define OUTPUT_DEV_VERSION          0x0111
 */
 
+/*
 // Emulates a steam controller
 #define OUTPUT_DEV_NAME             "Steam Controller"
 #define OUTPUT_DEV_VENDOR_ID        0x28de
 #define OUTPUT_DEV_PRODUCT_ID       0x1102
 #define OUTPUT_DEV_VERSION          0x0111
 #define OUTPUT_DEV_BUS_TYPE         BUS_USB
+*/
 
 /*
 //Emulates an Xbox one wireless controller:
 #define OUTPUT_DEV_NAME             "Xbox Wireless Controller"
 #define OUTPUT_DEV_VENDOR_ID        0x045e
 #define OUTPUT_DEV_PRODUCT_ID       0x028e
+#define OUTPUT_DEV_BUS_TYPE         BUS_BLUETOOTH
 */
 
-/*
+
 // Emulates a DualShock controller
 #define OUTPUT_DEV_NAME             "Sony Interactive Entertainment DualSense Wireless Controller"
 #define OUTPUT_DEV_VENDOR_ID        0x054c
 #define OUTPUT_DEV_PRODUCT_ID       0x0ce6
-*/
+#define OUTPUT_DEV_VERSION          0x8111
+#define OUTPUT_DEV_BUS_TYPE         BUS_USB
+
 
 #define PHYS_STR "00:11:22:33:44:55"
 
