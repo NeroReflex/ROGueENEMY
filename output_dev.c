3b2181c738de88a6b180479c4930a2dc78437243
Denis <benato.denis96@gmail.com>
2023-11-20 02:27:43 +0100

    mode switch is broken...

--- output_dev.c
+++ output_dev.c
@@ -538,6 +538,20 @@ static void decode_ev(output_dev_t *const out_dev, message_t *const msg) {
 	static int F15_status = 0;
 	static int gyroscope_mouse_translation = 0;
 
+	if (
+		(msg->data.event.ev[0].value == -13565784) &&
+		(msg->data.event.ev[1].type == EV_KEY) &&
+		(msg->data.event.ev[1].code == KEY_F18) &&
+		(msg->data.event.ev[1].value == 1) &&
+		(msg->data.event.ev_count >= 2) &&
+		(msg->data.event.ev[0].type == EV_MSC) &&
+		(msg->data.event.ev[0].code == MSC_SCAN)
+	) {
+		printf("Detected mode switch command, switching mode...\n");
+		cycle_mode(&out_dev->logic->platform);
+		//msg->flags |= INPUT_FILTER_FLAGS_DO_NOT_EMIT;
+	}
+
     if (msg->data.event.ev[0].type == EV_REL) {
         msg->data.event.ev_flags |= EV_MESSAGE_FLAGS_MOUSE;
     } else if ((msg->data.event.ev[0].type == EV_KEY) || (msg->data.event.ev[1].type == EV_KEY)) {
@@ -673,20 +687,6 @@ static void update_gs_from_ev(gamepad_status_t *const gs, message_t *const msg) 
 		printf("RC71L AC button short-press detected\n");
 #endif
 		gs->flags |= GAMEPAD_STATUS_FLAGS_PRESS_AND_REALEASE_CENTER;
-	} else if ((msg->data.event.ev[0].value == -13565784) && (msg->data.event.ev[1].type == EV_KEY) && (msg->data.event.ev[1].code == KEY_F18)) {
-		if (msg->data.event.ev[1].value == 1) {
-
-			if ((msg->data.event.ev_count >= 2) && (msg->data.event.ev[0].type == EV_MSC) && (msg->data.event.ev[0].code == MSC_SCAN)) {
-				printf("???\n");
-			} else {
-				printf("EXCUSE ME...");
-			}
-			//cycle_mode(&out_dev->logic->platform);
-		} else {
-			// Do nothing effectively discarding the input
-		}
-
-		//msg->flags |= INPUT_FILTER_FLAGS_DO_NOT_EMIT;
 	}
 
 	for (uint32_t i = 0; i < msg->data.event.ev_count; ++i) {
