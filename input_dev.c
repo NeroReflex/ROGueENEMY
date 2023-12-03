84452857c0d82574726af912175e3e9986a668f7
Denis <benato.denis96@gmail.com>
2023-12-03 22:24:15 +0100

    CPU usage optimization + fixed a non-existing leak (program cannot exit that function)

--- input_dev.c
+++ input_dev.c
@@ -568,7 +568,8 @@ static void input_udev(
             }
         }
 
-        const int timeout_ms = 1200;
+        // const int timeout_ms = 1200; 40% usage
+        const int timeout_ms = 5000; //Reduced from 1200 same functionality but cpu usage reduced by ~20%, any higher yield no sig results
 
         // while the incoming events thread run...
         while ((ctx->flags & INPUT_CTX_FLAGS_READ_TERMINATED) == 0) {
@@ -667,6 +668,12 @@ void *input_dev_thread_func(void *ptr) {
         }
 
         input_udev(in_dev, &ctx);
+
+        // free memory
+        for (int h = 0; h < MAX_MESSAGES_IN_FLIGHT; ++h) {
+            free(ctx.messages[h].data.event.ev);
+            ctx.messages[h].data.event.ev_size = 0;
+        }
     } else if (in_dev->dev_type == input_dev_type_iio) {
         // prepare space and empty messages
         for (int h = 0; h < MAX_MESSAGES_IN_FLIGHT; ++h) {
