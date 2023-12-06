8d01c9740166090d707058903cbb4717ba05febb
LtChipotle <diegocorando@gmail.com>
2023-12-05 21:01:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,29 +8,36 @@ SYSTEMD_DIR="/etc/systemd/system"
 UDEV_RULES_DIR="/usr/lib/udev/rules.d"
 CONFIG_DIR="/etc/ROGueENEMY"
 
+echo "Starting installation of ROGueENEMY..."
+
 # Ensure running as root
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root." >&2
     exit 1
 fi
 
-# Install dependencies
+echo "Installing dependencies..."
 # apt-get update
 # apt-get install -y cmake libconfig-dev libevdev libconfig git
 
 # Preparation
+echo "Preparing installation..."
 rm -rf "$BUILD_DIR"
 git clone "$REPO_URL" "$BUILD_DIR"
 
+echo "Copying udev rules..."
 cp "$BUILD_DIR/rogue_enemy.rule" "$UDEV_RULES_DIR/99-rogue_enemy.rules"
 cp "$BUILD_DIR/80-playstation.rules" "$UDEV_RULES_DIR"
 
+echo "Downloading the latest release..."
 curl -L $(curl -s https://api.github.com/repos/corando98/ROGueENEMY/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $BUILD_DIR/rogue-enemy
 
 # Installation
+echo "Installing executable..."
 mkdir -p "$INSTALL_DIR"
 cp "$BUILD_DIR/rogue-enemy" "$INSTALL_DIR/rogue-enemy"
 
+echo "Setting up system service..."
 mkdir -p "$SYSTEMD_DIR"
 mkdir -p "$UDEV_RULES_DIR"
 mkdir -p "$CONFIG_DIR"
@@ -41,6 +48,7 @@ install -m 644 "$BUILD_DIR/99-rogue_enemy.rules" "$UDEV_RULES_DIR/"
 # install -m 644 "$BUILD_DIR/config.cfg" "$CONFIG_DIR/config.cfg"
 
 # Post-installation
+echo "Reloading system daemons and applying new udev rules..."
 systemctl daemon-reload
 systemctl enable rogue-enemy.service
 systemctl start rogue-enemy.service
@@ -48,7 +56,7 @@ udevadm control --reload-rules
 udevadm trigger
 
 # Cleanup
+echo "Cleaning up..."
 rm -rf "$BUILD_DIR"
 
 echo "Installation complete."
-
